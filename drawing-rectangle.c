d37610e04f1330d33f176ab715363c87eb92ce2b
Ahnaf <107554314+ahnafrahmn@users.noreply.github.com>
2022-11-03 00:53:39 +0600

    Add files via upload

---
+++ drawing-rectangle.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d", &a);
+    c = a-1;
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=a-i; j++){
+            printf("  ");
+        }
+        if(i==1){
+            printf("* ");
+        } else if(i>1){
+            b = 2*i-2;
+            printf("* ");
+            for(int k=1; k<b; k++){
+                printf("  ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int p=1; p<=c; p++){
+        for(int q=0; q<p; q++){
+            printf("  ");
+        }
+        if(a==2){
+            printf("* ");
+        } else if(a>2){
+            printf("* ");
+            for(int r=1; r<=c-p; r++){
+                printf("    ");
+            }
+            printf("\b\b*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
