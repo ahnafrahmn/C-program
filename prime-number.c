d6d1012023c9cc02d9a75f11faa6312d4920d79d
Ahnaf <107554314+ahnafrahmn@users.noreply.github.com>
2022-12-10 19:13:27 +0600

    Update prime-number.c

--- prime-number.c
+++ prime-number.c
@@ -4,11 +4,21 @@
 
 int main()
 {
-    int num;
+    int num, i, prime=0;
     printf("enter a number : ");
     scanf("%d", &num);
-
-    if ((num == 1) || (num > 3) && ((num % 2 == 0) || (num % 3 == 0))){
+    
+    if ((num == 1) || (num == 0)){
+        printf("%d is not a prime number.", num);
+        return 0;
+    }
+    for(i=2; i<num; i++){
+        if(num%i==0){
+            prime++;
+            break;
+        }
+    }
+    if (prime==1){
         printf("%d is not a prime number.", num);
     } else {
         printf("%d is a prime number.", num);
